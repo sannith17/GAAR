28f69e14dc8ebd0104483ddffc44e384ad53e746
V Sannith Kumar <sannith17v@gmail.com>
2026-02-15 12:58:55 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="bg-primary text-white p-4 flex justify-between items-center">
-      <h1 className="text-3xl font-racing">TYREO</h1>
+      <h1 className="text-3xl font-racing">GAAR</h1>
       <div className="space-x-6">
         <Link href="/">Home</Link>
         <Link href="/cart">Cart</Link>
