91fb0e516db15e89d4b28014e668a0d54f77ca92
V Sannith Kumar <sannith17v@gmail.com>
2026-02-15 12:01:43 +0100

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-primary text-white p-6 text-center mt-10">
+      © 2026 TYREO Official Sales & Service
+    </footer>
+  );
+}
