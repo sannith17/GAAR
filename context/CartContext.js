3cadc09880d8c15c613822a75882406d30fbaa9a
V Sannith Kumar <sannith17v@gmail.com>
2026-02-15 13:54:52 +0100

    Update CartContext.js

--- context/CartContext.js
+++ context/CartContext.js
@@ -5,7 +5,11 @@ export const CartContext = createContext();
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
-  const addToCart = (product) => setCart([...cart, product]);
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+    alert(`${product.tyreBrand} added to cart!`); // simple popup
+  };
+
   const removeFromCart = (index) => {
     const updated = [...cart];
     updated.splice(index, 1);
