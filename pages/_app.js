cfbabc0e32e2e8aa367d3cd52ad15384dcdeede0
V Sannith Kumar <sannith17v@gmail.com>
2026-02-15 13:54:35 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
-import { CartProvider } from "../context/CartContext";
 import "../styles/globals.css";
+import { CartProvider } from "../context/CartContext";
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <CartProvider>
       <Component {...pageProps} />
     </CartProvider>
   );
 }
+
+export default MyApp;
