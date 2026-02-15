3f97ecb4b1cab88aff0933156871a89b786d2686
V Sannith Kumar <sannith17v@gmail.com>
2026-02-15 14:19:40 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,21 +2,19 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-primary shadow-md px-6 py-4 flex justify-between items-center">
+    <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
+      {/* Logo */}
       <Link href="/">
-        <a className="flex items-center gap-2">
-          <span className="inline-block w-14 h-14 rounded-full bg-gray-800 flex items-center justify-center text-white font-bold text-2xl">
-            G
-          </span>
-          <span className="font-schwarz text-4xl text-white">AAR</span>
-        </a>
+        <a className="font-schwarz text-5xl text-primary font-bold">GAAR</a>
       </Link>
+
+      {/* Menu */}
       <div className="flex gap-6">
         <Link href="/cart">
-          <a className="text-white hover:text-gray-300 font-semibold">Warenkorb</a>
+          <a className="text-gray-500 hover:text-gray-700 font-semibold">Warenkorb</a>
         </Link>
         <Link href="/contact">
-          <a className="text-white hover:text-gray-300 font-semibold">Kontakt</a>
+          <a className="text-gray-500 hover:text-gray-700 font-semibold">Kontakt</a>
         </Link>
       </div>
     </nav>
