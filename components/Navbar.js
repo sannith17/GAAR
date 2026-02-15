5b949d00570d58507de34e1a0a5a43f38ca8a369
V Sannith Kumar <sannith17v@gmail.com>
2026-02-15 12:00:19 +0100

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-primary text-white p-4 flex justify-between items-center">
+      <h1 className="text-3xl font-racing">TYREO</h1>
+      <div className="space-x-6">
+        <Link href="/">Home</Link>
+        <Link href="/cart">Cart</Link>
+      </div>
+    </nav>
+  );
+}
